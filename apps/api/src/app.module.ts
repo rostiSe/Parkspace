9949dccd44cfc7b15a8f6a3c3daf68a74eaf637c
rostise <you@example.com>
2024-09-08 19:03:47 +0200

    GraphQl added - RestApi Swagger Added- Auth added - Tested

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,19 +5,31 @@ import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path'
 import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from './common/prisma/prisma.module'
+import { UsersModule } from './models/users/users.module'
+import { JwtModule } from '@nestjs/jwt'
+
+// TODO; Mover to util library
+const MAX_AGE = 60 * 60 * 24 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: MAX_AGE },
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver : ApolloDriver,
+      driver: ApolloDriver,
       introspection: true,
       fieldResolverEnhancers: ['guards'],
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       buildSchemaOptions: {
-        numberScalarMode: 'integer'
-      }
+        numberScalarMode: 'integer',
+      },
     }),
+    PrismaModule, UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
+
+
+  const config = new DocumentBuilder()
+    .setTitle('Parkspace | Rostislaw Sergejew')
+    .setDescription(
+      `<div>
+        <p><a target="__blanc" href="http://localhost:3000/graphql">Here</a> to get to the GraphQl Editor. Use 
+      <a target="__blanc" href="https://studio.apollographql.com/sandbox/explorer?endpoint=http://localhost:3000/graphql&document=query users{users{uid}}">Apollo</a>
+        for a Better GraphQL Experience.</p>
+        </div>
+      `
+    )
+    .setVersion('0.1')
+    .addTag('parkspace')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/', app, document)
+
+
   await app.listen(3000)
 }
 bootstrap()
